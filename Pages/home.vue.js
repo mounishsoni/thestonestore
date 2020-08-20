59a72326c357082f7aae9899c899fd9875344169
Parth <parthchauhan05@gmail.com>
2020-08-20 16:31:32 -0400

    Initial Project Setup

---
+++ Pages/home.vue.js
@@ -0,0 +1,19 @@
+const Home = {
+    data: function() {
+        return {
+            
+        }
+    },
+    template: `
+    <section>
+        <div class="row py-3">
+            <div class="col"></div>
+            <div class="col-md-10 text-center">
+                <h1 class="montserrat">The Stone Store</h1>
+                <h3>Coming Soon...</h3>
+            </div>
+            <div class="col"></div>
+        </div>
+    </section>
+  `
+}
