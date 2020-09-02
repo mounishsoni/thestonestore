43ae1455c44c92adfc52de28928952d6081da715
Parth <parthchauhan05@gmail.com>
2020-09-02 18:17:52 -0400

    Contact Us Page | SendMail | Route set to Contact

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const routes = [
     {path: '/', name: 'Home', component: homeComponent},
+    {path: '/contact', name: 'Contact Us', component: contactComponent},
     {path: '/signin', name: 'Sign In', component: signInComponent},
     {path: '/signup', name: 'Sign Up', component: signUpComponent}
 ]
