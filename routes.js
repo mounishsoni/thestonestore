b278c5f4564dfa44793816eb4a2740917a5d4d4c
Parth <parthchauhan05@gmail.com>
2020-09-15 18:46:27 -0400

    Add Category and Add Tag Forms working

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const routes = [
     {path: '/', name: 'Home', component: homeComponent},
+    {path: '/admin/addCategory', name: 'Add Category', component: addCategoryComponent},
+    {path: '/admin/addTag', name: 'Add Tag', component: addTagComponent},
     {path: '/contact', name: 'Contact Us', component: contactComponent},
     {path: '/signin', name: 'Sign In', component: signInComponent},
     {path: '/signup', name: 'Sign Up', component: signUpComponent}
